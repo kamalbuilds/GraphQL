43690d67ebc4166e8e2c2822a6509f102877a055
Kamal Nayan <geniusamansingh@gmail.com>
2022-08-23 08:15:06 +0000

    typedef and reslovers defined

---
+++ resolver.js
@@ -0,0 +1,7 @@
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+export const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
